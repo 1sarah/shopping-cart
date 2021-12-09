f0ed9640ef8d0fa623bfe48d80bcc6003937243d
1sarah <mbuyaeugenis@gmail.com>
2021-12-09 17:47:42 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import img1 from './images/images.jpeg';
+const data ={
+     productData: [
+         {
+       id:1,
+       img:img1,
+       title:'Product 1',
+       desc:"",
+       price:100,
+         },
+         {
+            id:2,
+            img:img1,
+            title:'Product 12',
+            desc:"",
+            price:100,
+              },
+              {
+                id:1,
+                img:img1,
+                title:'Product 3',
+                desc:"",
+                price:100,
+                  }
+        ],
+};
+export default data;
